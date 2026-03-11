7d6c95da44976b89c312547166d11f28f5400222
bernardoivoo <99845804+bernardoivoo@users.noreply.github.com>
2026-03-11 16:07:14 -0300

    dto

---
+++ src/main/java/Dto/AgendarConsultaDto.java
@@ -0,0 +1,44 @@
+package clinica.dto;
+
+import java.time.LocalDateTime;
+
+public class AgendarConsultaDTO {
+
+    private Long pacienteId;
+    private Long medicoId;
+    private LocalDateTime dataHora;
+    private String observacoes;
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public Long getMedicoId() {
+        return medicoId;
+    }
+
+    public void setMedicoId(Long medicoId) {
+        this.medicoId = medicoId;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+}
+
